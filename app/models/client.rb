6d20c0763ec1481ad8b3e685102e623a2fc3efc2
NEWESTERS <ramiro.cruz@mail.ru>
2017-12-09 04:38:34 +0300

    Design improvments

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,13 @@
 class Client < ApplicationRecord
 	has_many :seances
+	validates :name, format: { with: /\A[а-яА-Я]+[ -]?[а-яА-Я]*\z/, message: "Should contain only letters" }
+	validates :last_name, format: { with: /\A[а-яА-Я]+[ -]?[а-яА-Я]*\z/, message: "Should contain only letters" }
+	validates :patronymic, format: { with: /\A[а-яА-Я]+[ -]?[а-яА-Я]*\z/, message: "Should contain only letters" }
+	validates :birthdate, presence: true
+	validates :phone, format: { with: /\A[+][7,8][ ][(][0-9]{3}[)][ ][0-9]{3}[-][0-9]{2}[-][0-9]{2}+\z/, message: "Should be valid phone" }
+	before_save :convert_case
 
 	def full_name
-		self.last_name + ' ' + self.name + ' ' + self.patronymic
+		last_name + ' ' + name + ' ' + patronymic
 	end
 end
