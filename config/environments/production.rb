d7b34cf367c9ba6413236bf8c0c77073b500c56f
NEWESTERS <ramiro.cruz@mail.ru>
2018-01-10 15:24:15 +0300

    SMTP fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@npcpn.com'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'newwwesters@gmail.com',
+    password: '25881234',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
