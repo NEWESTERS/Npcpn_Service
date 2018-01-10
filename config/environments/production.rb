8529a0f92e1c017173c1f4f827e57a34a0ebc520
NEWESTERS <ramiro.cruz@mail.ru>
2018-01-10 15:28:31 +0300

    SMTP fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@npcpn.com'}
+  config.action_mailer.default_options = {from: 'newwwesters@gmail.com'}
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
